d0dbb16eeedee83aac62917594adbaf1b8a9c77b
Boadu Akoto Charles Jnr <ekowcharles@yahoo.co.uk>
2015-09-15 17:01:46 -0300

    Sets state of newly added treasure as pending

--- app/models/treasure.rb
+++ app/models/treasure.rb
@@ -1,2 +1,16 @@
 class Treasure < ActiveRecord::Base
+  before_create :set_default_status
+
+  DONE = 'DONE'
+  CLOSED = 'CLOSED'
+  PENDING = 'PENDING'
+
+  def self.states
+    { 'DONE' => DONE, 'PENDING' => PENDING, 'CLOSED' => CLOSED }
+  end
+
+  private  
+  def set_default_status
+    self.status = PENDING
+  end
 end
