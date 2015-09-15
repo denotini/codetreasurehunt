17f1c775eda31531831b7311d0b1ea628aab84b9
Boadu Akoto Charles Jnr <ekowcharles@yahoo.co.uk>
2015-09-15 18:17:12 -0300

    Puts table to left of image

--- app/helpers/treasures_helper.rb
+++ app/helpers/treasures_helper.rb
@@ -1,2 +1,14 @@
 module TreasuresHelper
+  def toggle_tab(status)
+    if status == safify(params[:status])
+      'class=active'
+    else
+      ''
+    end
+  end
+
+  private
+  def safify(value)
+    value.nil? ? nil : value.upcase
+  end
 end
